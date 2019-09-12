ad41185c0311d44f1e7805a24e0503dbf8e5c266
Haiko Schol <hs@haikoschol.com>
2019-09-12 14:29:06 +0200

    Take inspiration from better-defaults

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(package-selected-packages (quote (all-the-icons neotree rg magit base16-theme))))
+ '(package-selected-packages
+   (quote
+    (uniquify-files rg all-the-icons neotree magit base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,18 +1,14 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
-(setq inhibit-splash-screen t)
 (setq make-backup-files nil)
-
+(setq inhibit-splash-screen t)
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (global-display-line-numbers-mode)
+(show-paren-mode 1)
+(save-place-mode 1)
 
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
@@ -20,11 +16,25 @@
 (setq custom-file (concat dotfiles-dir "custom.el"))
 (load custom-file 'noerror)
 
+(setq-default indent-tabs-mode nil)
+(setq mouse-yank-at-kill t)
+(setq require-final-newline t)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq save-place-file (concat dotfiles-dir "places"))
+
+(setq ido-decorations
+      '("\n=> " "" "\n" "" "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]"))
+
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
 	("melpa" . "https://melpa.org/packages/")))
 
+(package-install-selected-packages)
+
 (load-theme 'base16-materia t)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(setq neo-window-fixed-size nil)
+(setq uniquify-buffer-name-style 'forward)
 
 (rg-enable-default-bindings)
 
@@ -34,5 +44,10 @@
 		  (join-line -1)))
 
 (global-set-key [f8] 'neotree-toggle)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
