d526e1a32ade69415e6d8bed5a5598fc339915b1
Haiko Schol <539509+haikoschol@users.noreply.github.com>
2021-01-29 20:15:41 +0100

    hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,7 @@
+require "hs.caffeinate"
+require "hs.hotkey"
+
+hs.caffeinate.set("displayIdle", true, false)
+hs.caffeinate.set("systemIdle", true, false)
+hs.caffeinate.set("system", true, false)
+
--- mklinks.sh
+++ mklinks.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
 ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/zprofile ~/.zprofile
+ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
 
-mkdir -p ~/.config/sway
-ln -s ~/dotfiles/sway/config ~/.config/sway/config
+#ln -s ~/dotfiles/zprofile ~/.zprofile
 
-mkdir -p ~/.config/i3blocks
-ln -s ~/dotfiles/i3blocks/config ~/.config/i3blocks/config
-
-mkdir -p ~/.config/alacritty
-ln -s ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-
-mkdir -p ~/.config/albert
-ln -s ~/dotfiles/albert/albert.conf ~/.config/albert/albert.conf
-
-mkdir -p ~/.config/redshift
-ln -s ~/dotfiles/redshift/redshift.conf ~/.config/redshift/redshift.conf
-
-mkdir -p ~/.config/systemd/user
-# ln -s ~/dotfiles/systemd/sway-session.target ~/.config/systemd/user/sway-session.target
-ln -s ~/dotfiles/systemd/wallpaper.service ~/.config/systemd/user/wallpaper.service
-ln -s ~/dotfiles/systemd/wallpaper.timer ~/.config/systemd/user/wallpaper.timer
-# ln -s ~/dotfiles/systemd/redshift.service ~/.config/systemd/user/redshift.service
-
-ln -s ~/dotfiles/emacs.d ~/.emacs.d
-
-mkdir ~/bin
-ln -s ~/dotfiles/bin/change_wallpaper.sh ~/bin/change_wallpaper.sh
-ln -s ~/dotfiles/bin/update_everything.sh ~/bin/update_everything.sh
-
-ln -s ~/dotfiles/Xresources ~/.Xresources
-
+#mkdir -p ~/.config/sway
+#ln -s ~/dotfiles/sway/config ~/.config/sway/config
+#
+#mkdir -p ~/.config/i3blocks
+#ln -s ~/dotfiles/i3blocks/config ~/.config/i3blocks/config
+#
+#mkdir -p ~/.config/alacritty
+#ln -s ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+#
+#mkdir -p ~/.config/albert
+#ln -s ~/dotfiles/albert/albert.conf ~/.config/albert/albert.conf
+#
+#mkdir -p ~/.config/redshift
+#ln -s ~/dotfiles/redshift/redshift.conf ~/.config/redshift/redshift.conf
+#
+#mkdir -p ~/.config/systemd/user
+## ln -s ~/dotfiles/systemd/sway-session.target ~/.config/systemd/user/sway-session.target
+#ln -s ~/dotfiles/systemd/wallpaper.service ~/.config/systemd/user/wallpaper.service
+#ln -s ~/dotfiles/systemd/wallpaper.timer ~/.config/systemd/user/wallpaper.timer
+## ln -s ~/dotfiles/systemd/redshift.service ~/.config/systemd/user/redshift.service
+#
+#ln -s ~/dotfiles/emacs.d ~/.emacs.d
+#
+#mkdir ~/bin
+#ln -s ~/dotfiles/bin/change_wallpaper.sh ~/bin/change_wallpaper.sh
+#ln -s ~/dotfiles/bin/update_everything.sh ~/bin/update_everything.sh
+#
+#ln -s ~/dotfiles/Xresources ~/.Xresources
+#
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
