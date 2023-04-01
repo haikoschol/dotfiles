729e73e4bc595eb6b101cca622fc02e16b268b09
Haiko Schol <539509+haikoschol@users.noreply.github.com>
2023-04-01 11:34:10 +0700

    improve mklinks.sh

--- mklinks.sh
+++ mklinks.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
+! [[ -e ~/.zshrc ]] && -ln -s ~/dotfiles/zshrc ~/.zshrc
+! [[ -e ~/.tmux.conf ]] && ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+! [[ -e ~/.vim ]] && ln -s ~/dotfiles/vim ~/.vim
+! [[ -e ~/.vimrc ]] && ln -s ~/dotfiles/vimrc ~/.vimrc
+! [[ -e ~/.hammerspoon ]] && ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
 
-#ln -s ~/dotfiles/zprofile ~/.zprofile
+mkdir -p ~/.config/alacritty
+! [[ -e ~/.config/alacritty/alacritty.yml ]] && ln -s ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-#mkdir -p ~/.config/sway
-#ln -s ~/dotfiles/sway/config ~/.config/sway/config
-#
-#mkdir -p ~/.config/i3blocks
-#ln -s ~/dotfiles/i3blocks/config ~/.config/i3blocks/config
-#
-#mkdir -p ~/.config/alacritty
-#ln -s ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-#
-#mkdir -p ~/.config/albert
-#ln -s ~/dotfiles/albert/albert.conf ~/.config/albert/albert.conf
-#
-#mkdir -p ~/.config/redshift
-#ln -s ~/dotfiles/redshift/redshift.conf ~/.config/redshift/redshift.conf
-#
-#mkdir -p ~/.config/systemd/user
-## ln -s ~/dotfiles/systemd/sway-session.target ~/.config/systemd/user/sway-session.target
-#ln -s ~/dotfiles/systemd/wallpaper.service ~/.config/systemd/user/wallpaper.service
-#ln -s ~/dotfiles/systemd/wallpaper.timer ~/.config/systemd/user/wallpaper.timer
-## ln -s ~/dotfiles/systemd/redshift.service ~/.config/systemd/user/redshift.service
-#
-#ln -s ~/dotfiles/emacs.d ~/.emacs.d
-#
-#mkdir ~/bin
-#ln -s ~/dotfiles/bin/change_wallpaper.sh ~/bin/change_wallpaper.sh
-#ln -s ~/dotfiles/bin/update_everything.sh ~/bin/update_everything.sh
-#
-#ln -s ~/dotfiles/Xresources ~/.Xresources
-#
+mkdir -p ~/bin
+! [[ -e ~/bin/yank ]] && ln -s ~/dotfiles/bin/yank ~/bin/yank
+
+if [[ $(uname) != "Darwin" ]]; then
+    mkdir -p ~/.config/sway
+    ! [[ -e ~/.config/sway/config ]] && ln -s ~/dotfiles/sway/config ~/.config/sway/config
+
+    mkdir -p ~/.config/i3blocks
+    ! [[ -e ~/.config/i3blocks/config ]] && ln -s ~/dotfiles/i3blocks/config ~/.config/i3blocks/config
+
+    mkdir -p ~/.config/albert
+    ! [[ -e ~/.config/albert/albert.conf ]] && ln -s ~/dotfiles/albert/albert.conf ~/.config/albert/albert.conf
+
+    mkdir -p ~/.config/redshift
+    ! [[ -e ~/.config/redshift/redshift.conf ]] && ln -s ~/dotfiles/redshift/redshift.conf ~/.config/redshift/redshift.conf
+
+    mkdir -p ~/.config/systemd/user
+    ! [[ ~/.config/systemd/user/sway-session.target ]] && ln -s ~/dotfiles/systemd/sway-session.target ~/.config/systemd/user/sway-session.target
+    ! [[ -e ~/.config/systemd/user/wallpaper.service ]] && ln -s ~/dotfiles/systemd/wallpaper.service ~/.config/systemd/user/wallpaper.service
+    ! [[ -e ~/.config/systemd/user/wallpaper.timer ]] && ln -s ~/dotfiles/systemd/wallpaper.timer ~/.config/systemd/user/wallpaper.timer
+    ! [[ -e ~/.config/systemd/user/redshift.service ]] && ln -s ~/dotfiles/systemd/redshift.service ~/.config/systemd/user/redshift.service
+
+    ! [[ -e ~/bin/change_wallpaper.sh ]] && ln -s ~/dotfiles/bin/change_wallpaper.sh ~/bin/change_wallpaper.sh
+    ! [[ -e ~/bin/update_everything.sh ]] && ln -s ~/dotfiles/bin/update_everything.sh ~/bin/update_everything.sh
+
+    ! [[ -e ~/.Xresources ]] && ln -s ~/dotfiles/Xresources ~/.Xresources
+fi
